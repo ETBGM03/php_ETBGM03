c37883a8c4c638f5a880a5a453176aadd59cd976
ETBGM03 <56968883+ETBGM03@users.noreply.github.com>
2020-04-26 21:26:37 -0500

    terminado
    falta añadirle estilos

---
+++ php-ETBGM03/ejercicio4/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>presupuesto</title>
+</head>
+
+<body>
+    <h1>Repartición presupuestal</h1>
+    <form action="pagina5.php" method="post">
+        Digite presupuesto
+        <label><input type="text" name='presupuesto'></label> <br>
+        
+        <br>
+        <label><input type="submit" value="enviar"></label>
+
+
+    </form>
+
+ 
+</body>
+
+</html>
---
+++ php-ETBGM03/ejercicio4/pagina5.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Repartido</title>
+</head>
+
+<body>
+    <h1>Porcentaje de cada departamento</h1>
+    <?php
+
+    $presupuesto = $_POST['presupuesto'];
+    $pediatria = $presupuesto * 0.40;
+    $pginecol = $presupuesto * 0.30;
+    $taruma = $presupuesto * 0.30;
+
+
+
+    ?>
+    <?php
+    echo 'el presupuesto para pediatra es:' .  $pediatria .'<br>';
+    echo 'el presupuesto para Traumatología es:' .  $pginecol .'<br>';
+    echo 'el presupuesto para Ginecología es:' .  $taruma;
+
+
+    ?>
+
+</body>
+
+</html>
