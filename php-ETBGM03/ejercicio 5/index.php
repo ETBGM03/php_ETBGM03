a2e1c374a6e9ed2464e392560f1015a99b0f7b9f
ETBGM03 <56968883+ETBGM03@users.noreply.github.com>
2020-05-12 21:04:59 -0500

    Corregido
    se añadió estilos y corrigió los errores

--- php-ETBGM03/ejercicio 5/index.php
+++ php-ETBGM03/ejercicio 5/index.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stilos.css">
-    <title>Document</title>
+    <title>Artículos</title>
 </head>
+
 <body>
-    <h1>Tienda online</h1>
-    <form action="pagina7.php" method="post">
-        Digite el artículo
-        <label><input type="text" name='articulo'></label> <br>
-        Digite la clave
-        <label><input type="text" name='claveP'></label> <br>
-        Digite precio
-        <label><input type="text" name='precioOri'></label> <br>
-        <label ><input type="submit" value='enviar'></label>
+    <div class="lol">
+        <h1>Tienda online</h1>
+        <form action="pagina7.php" method="post">
+            Digite el artículo
+            <label><input class="asd" type="text" name='articulo'></label> <br>
+            Digite la clave
+            <label><input class="asd03" type="text" name='claveP'></label> <br>
+            Digite precio
+            <label><input class="asd02" type="text" name='precioOri'></label> <br>
+            <label><input class="io" type="submit" value='enviar'></label>
+        </form>
+    </div>
 
-
-
-
-
-
-    </form>
 </body>
+
 </html>
--- php-ETBGM03/ejercicio 5/pagina7.php
+++ php-ETBGM03/ejercicio 5/pagina7.php
@@ -1,38 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estiless.css">
-    <title>Document</title>
+    <title>Tú descuento</title>
 </head>
+
 <body>
-    <h1>Super  descuento</h1>
-    <?php
-        
-        $articulo = $_POST ['articulo'];
-        $precio = $_POST ['precioOri'];
-        $claveP = $_POST ['claveP'];
-        $descuento1=($precio*0.10);
-        $descuento2=($precio*0.20);
-        $p1=($precio-$descuento1);
-        $p2=($precio-$descuento2);
+
+    <div class="juo">
+        <h1>Super descuento</h1>
+        <?php
+
+        $articulo = $_POST['articulo'];
+        $precio = $_POST['precioOri'];
+        $claveP = $_POST['claveP'];
+        $descuento1 = ($precio * 0.10);
+        $descuento2 = ($precio * 0.20);
+        $p1 = ($precio - $descuento1);
+        $p2 = ($precio - $descuento2);
         if ($claveP == 1) {
             echo 'el valor del articulo con el descuento es: ' . $p1 . '<br>';
-        }
-        elseif ($claveP == 2) {
+        } elseif ($claveP == 2) {
             echo 'el valor del articulo con el descuento es: ' . $p2 . '<br>';
-        }
-        else {
-            echo 'usted no tiene ningun descuento';
+        } else {
+            echo 'usted no tiene ningun descuento' . '<br>';
         }
 
-        
-    ?>
-    <?php
-        echo 'el nombre del articulo es: ' . $articulo . '<br>'; 
+
+        ?>
+        <?php
+        echo 'el nombre del articulo es: ' . $articulo . '<br>';
         echo 'el precio del articulo es: ' . $precio . '<br>';
         echo 'la clave del articulo es: ' . $claveP . '<br>';
-    ?>
+        ?>
+
+
+    </div>
+
 </body>
+
 </html>
